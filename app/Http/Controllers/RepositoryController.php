de9257aa8492b63cf0c95a4e662e0d73c5694ee4
Claudio Rivera Bravo <riverabravocl@outlook.com>
2023-10-11 23:11:08 -0300

    actualizar registro

--- app/Http/Controllers/RepositoryController.php
+++ app/Http/Controllers/RepositoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Repository;
 use Illuminate\Http\Request;
 
 class RepositoryController extends Controller
@@ -11,5 +12,12 @@ class RepositoryController extends Controller
         $request->user()->repositories()->create($request->all()); //Estamos diciendo que cree un elemento con el metodo de las relaciones usando el id del usuario logueadode manera masiva.
 
         return redirect()->route('repositories.index');            //Retornamos a una ruta definida y la vista index una ves creado.
+    }
+
+    public function update(Request $request,Repository $repository) //Vamos a recibir todo lo que enviamos a traves del form mediante un request y el ropositorio.
+    {
+        $repository->update($request->all());                       //Actualizamos el repositorio seleccionado de manera masiva
+
+        return redirect()->route('repositories.edit',$repository);    //Retornamos a una ruta definida y la vista respository una ves creado y nos muestre la actualizacion.
     }
 }
--- tests/Feature/Http/Controllers/RepositoryControllerTest.php
+++ tests/Feature/Http/Controllers/RepositoryControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Http\Controllers;
 
+use App\Models\Repository;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -38,9 +39,29 @@ class RepositoryControllerTest extends TestCase
         $this
             ->actingAs($user)                           //Iniciaremos sesion y le diremos actua como el usuario que acabamos de crear.
             ->post('repositories', $data)               //Lo enviamos al servidor mediante post, a la ruta que ya configuramos('repositories') a traves del formulario que creamos:($data)
-            ->assertRedirect('repositories');           //Redireccionamos a ka vista del repo:('repositoires')
+            ->assertRedirect('repositories');           //Redireccionamos a la vista del repo:('repositoires')
 
         $this
             ->assertDatabaseHas('repositories', $data); //Verificamos la informacion en la DB, tabla('repositories') 
     }
+
+    public function test_update()
+    {
+        $repository = Repository::factory()->create();  //Creamos un elemento repositorio.
+        $data = [
+            'url' => $this->faker->url,
+            'description' => $this->faker->text,        //Nuestro formulario queremos salvar una url y una descripcion.
+        ];
+
+        $user = User::factory()->create();              //Creamos un usuario que iniciara sesion.
+
+        $this
+            ->actingAs($user)                                      //Iniciaremos sesion y le diremos actua como el usuario que acabamos de crear.
+            ->put("repositories/$repository->id", $data)           //Actulizamos mediante put, como recibiremos variables utilizamos "repositories", cuyo id sea el que ingresaremos:($repository->id)
+            ->assertRedirect("repositories/$repository->id/edit"); //Redireccionamos a la vista del repo:('repositoires')
+
+        $this
+            ->assertDatabaseHas('repositories', $data);            //Verificamos la informacion en la DB, tabla('repositories') 
+    }
+
 }
