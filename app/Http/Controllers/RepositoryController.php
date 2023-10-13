5e15548092a23c190298aa4e9c4a9f752854d5fd
Claudio Rivera Bravo <riverabravocl@outlook.com>
2023-10-13 15:49:42 -0300

    validación de datos

--- app/Http/Controllers/RepositoryController.php
+++ app/Http/Controllers/RepositoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RepositoryRequest;
 use App\Models\Repository;
 use Illuminate\Http\Request;
 
@@ -45,24 +46,15 @@ class RepositoryController extends Controller
    }
 
     
-    public function store(Request $request)                        //Vamos a recibir todo lo que enviamos a traves del form mediante un request.
+    public function store(RepositoryRequest $request)                        //Vamos a recibir todo lo que enviamos a traves del form mediante un request.
     {
-        $request->validate([
-            'url' => 'required',
-            'description' => 'required'
-        ]);
         $request->user()->repositories()->create($request->all()); //Estamos diciendo que cree un elemento con el metodo de las relaciones usando el id del usuario logueadode manera masiva.
 
         return redirect()->route('repositories.index');            //Retornamos a una ruta definida y la vista index una ves creado.
     }
 
-    public function update(Request $request,Repository $repository) //Vamos a recibir todo lo que enviamos a traves del form mediante un request y el ropositorio.
+    public function update(RepositoryRequest $request,Repository $repository) //Vamos a recibir todo lo que enviamos a traves del form mediante un request y el ropositorio.
     {
-        $request->validate([
-            'url' => 'required',
-            'description' => 'required'
-        ]);
-
         if ($request->user()->id != $repository->user_id )          //El usuario que esta intentando actulizar es dueño del repositorio?.
         {
             abort(403);                                             //Si no es dueño arroja un error 403.
@@ -85,8 +77,4 @@ class RepositoryController extends Controller
 
         return redirect()->route('repositories.index');  //Retornamos a una ruta definida y la vista respository una ves creado y nos muestre la actualizacion.
     }
-
-
-
-
 }
